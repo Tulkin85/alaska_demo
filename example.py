99b2e45d68facc988c108ec17ff0c877bb6ed9d6
Tulkin85 <tulkinniyazov@gmail.com>
2025-04-25 12:58:53 -0400

    Alaska_demo_update new file:   .gitignore new file:   .vscode/settings.json new file:   databricks.yml new file:   example.py new file:   example_notebook.py

---
+++ example.py
@@ -0,0 +1,9 @@
+from pyspark.sql.functions import *
+
+table = 'samples.nyctaxi.trips'
+
+df = spark.read.table(table)
+
+df.select("tpep_pickup_datetime", "tpep_dropoff_datetime",
+          "trip_distance","fare_amount", "pickup_zip", "dropoff_zip").limit(100).show()
+#this is vscode
